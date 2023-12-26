a8f1ee1b249b55acacbfd41411424594db47fc75
david-s-schutte <david.schutte01@gmail.com>
2023-12-26 16:14:28 +1100

    C++ Warmup - create a basic script that rotates a block

---
+++ Source/GameProject23to24/Private/PickupRotator.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickupRotator.h"
+
+// Sets default values for this component's properties
+UPickupRotator::UPickupRotator()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPickupRotator::BeginPlay()
+{
+	Super::BeginPlay();
+	// ...
+	
+}
+
+
+// Called every frame
+void UPickupRotator::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	TickRotateObject(DeltaTime);
+	// ...
+}
+
+void UPickupRotator::TickRotateObject(const float& DeltaTime)
+{
+	FRotator CurrentActorRotation = GetOwner()->GetActorRotation();
+	CurrentActorRotation.Yaw += DeltaTime * RotSpeed;
+	GetOwner()->SetActorRotation(CurrentActorRotation);
+}
+
---
+++ Source/GameProject23to24/Public/PickupRotator.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PickupRotator.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GAMEPROJECT23TO24_API UPickupRotator : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPickupRotator();
+
+private:
+	UPROPERTY(EditInstanceOnly);
+	float RotSpeed;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void TickRotateObject(const float& DeltaTime);
+		
+};
